a744bfd07205a9823ef7f9a47437b0945b3956a9
CuberDesign <cuber.design.solutions@gmail.com>
2024-02-27 03:32:41 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KanbanBoard
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
